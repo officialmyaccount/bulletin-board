c6ce0e4d07871c97f56a369b561a8ea141b98dad
officialmyaccount <bicebc@gmail.com>
2023-07-03 19:05:01 +0900

    HTMLファイルの記述

--- bbs/urls.py
+++ bbs/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+from .models import Article
+from django.views import generic
 
 app_name = 'bbs'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>/', views.detail, name='detail'),
+    # path('', views.index, name='index'),
+    # path('<int:id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.create, name='create'),
     path('<int:id>/update', views.update, name='update'),
     path('<int:id>/delete', views.delete, name='delete'),
--- bbs/views.py
+++ bbs/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
+from django.views import generic
+
+class IndexView(generic.ListView):
+    model = Article
+
+class DetailView(generic.DetailView):
+    model = Article
 
 def index(request):
     return HttpResponse('Hello Django')
